93d87a8f749e479c9fa3cdce9788767f9c3078a2
Ben-Mostyn <ben_mostyn@live.co.uk>
2022-11-09 14:40:58 +0000

    Functionality Finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DisplayBlock from "./component/DisplayBlock";
+import PriceEntryField from "./component/PriceEntryField";
+import VatRateField from "./component/VatRateField";
+import { useState } from "react";
 
 function App() {
+  // States
+  const [netPrice, setNetPrice] = useState(0.0);
+  const [grossPrice, setGrossPrice] = useState(0.0);
+  const [vatToPay, setVatToPay] = useState(0);
+  const [vatRate, setVatRate] = useState(20);
+
+  // Functions
+  const handleNetPriceChange = (price) => {
+    const grossPrice = price * (vatRate / 100 + 1);
+    setNetPrice(price);
+    setGrossPrice(grossPrice);
+    //TODO: calc vat to pay and set state
+    setVatToPay(grossPrice - price);
+  };
+
+  const handleGrossPriceChange = (price) => {
+    const netPrice = price / (vatRate / 100 + 1);
+    setNetPrice(netPrice);
+    setGrossPrice(price);
+    //TODO: calc vat to pay and set state
+    setVatToPay(price - netPrice);
+  };
+
+  const handleVatRateChange = (rate) => {
+    setVatRate(rate);
+    handleNetPriceChange(netPrice);
+  };
+
+  const updatePrices = () => {
+    handleNetPriceChange(netPrice);
+  };
+
+  // Return
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header field">
+      <h1>VAT CALCULATOR</h1>
+      <div className="pale-green-border">
+        <VatRateField
+          vatRateChanged={handleVatRateChange}
+          customstyle="field"
+          updatePrices={updatePrices}
+          value={vatRate}
+        />
+        <PriceEntryField
+          customstyle="field"
+          label="Price excl VAT: "
+          price={netPrice === 0.0 ? "" : netPrice}
+          priceChanged={handleNetPriceChange}
+        />
+        <DisplayBlock
+          customstyle="field"
+          value={vatToPay}
+          label="Vat to pay: "
+        />
+        <PriceEntryField
+          customstyle="field"
+          label="Price incl VAT: "
+          price={grossPrice === 0.0 ? "" : grossPrice}
+          priceChanged={handleGrossPriceChange}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/component/NumberEditorField.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NumberEditorField = (props) => {
+  return (
+    <input
+      type="number"
+      id="num"
+      value={props.value}
+      onChange={(event) => {
+        props.valueChanged(event.target.value);
+      }}
+    />
+  );
+};
+
+export default NumberEditorField;
